79d8efd85e66581491eff2a0d7137b84181917c7
Jason Peak <jpeak5@lsu.edu>
2017-06-16 11:47:05 -0500

    auto-agrees to apt-get install, enables php display_errors.

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 sudo apt-get update
-apt-get install libapache2-mod-php5 git emacs php-elisp imagemagick php5-xsl
+apt-get -y install libapache2-mod-php5 git emacs php-elisp imagemagick php5-xsl
 rm -rf /var/www/html
 cp -r /vagrant/site/html /var/www/
 #git clone https://github.com/blekinge/altoviewer /var/www/html
+echo "display_errors = On" > /etc/php5/apache2/conf.d/05-dev.ini
 apachectl restart
 #convert mn_19231115_001.jp2 mn_19231115_001.jpg
