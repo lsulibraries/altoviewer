5536dfb49db245bbd869eeb94995df301b6ba9d2
Jason Peak <jpeak5@lsu.edu>
2017-06-16 14:59:34 -0500

    adds xdebug to build

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
 sudo apt-get update
-apt-get -y install libapache2-mod-php5 git emacs php-elisp imagemagick php5-xsl
+apt-get -y install libapache2-mod-php5 git emacs php-elisp imagemagick php5-xsl php5-xdebug
 rm -rf /var/www/html
 cp -r /vagrant/site/html /var/www/
 #git clone https://github.com/blekinge/altoviewer /var/www/html
 echo "display_errors = On" > /etc/php5/apache2/conf.d/05-dev.ini
+<<EOF
+zend_extension="/usr/lib/php5/20121212/xdebug.so"
+xdebug.remote_enable=1
+xdebug.remote_handler=dbgp 
+xdebug.remote_mode=req
+xdebug.remote_host=130.39.60.175 
+xdebug.remote_port=9000
+xdebug.max_nesting_level=300
+EOF
+
 apachectl restart
 #convert mn_19231115_001.jp2 mn_19231115_001.jpg
